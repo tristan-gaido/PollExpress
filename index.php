ad273c503569afcb7b57823888c4d4b49fb40ac9
tristan-gaido <78071501+tristan-gaido@users.noreply.github.com>
2021-10-17 18:47:55 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,79 @@
-<!doctype html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8">
-  <title>PollExpress</title>
-  <link rel="stylesheet" href="style.css">
-  <script src="script.js"></script>
-</head>
-<body>
-  <p>TEST toutnouveau HTML<p>
-</body>
+<?php
+	session_start();
+
+	include('./BDD.php');
+
+	if (!isset($_SESSION['id'])){   //Si l'utilisateur n'est pas connecté, il est redirigé automatiquement vers la page de login
+    header('Location: login.php');
+    exit;
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+  ﻿<head>
+    ﻿<meta charset="utf-8"/>
+    ﻿<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+	  <link rel="stylesheet" href="styles.css">
+	  <script src="script.js"></script>	
+  ﻿  <title>PollExpress</title>
+  </head>
+	<header>
+	  <div class="header">
+	    <div id="logo" class="header-item">
+	      <h1>PollExpress </h1>
+	      <img src="logo.png" alt="PollExpress Logo">
+	    </div>
+	    <div class="header-item">
+	      	<?php
+	if(isset($_SESSION['id'])){
+		?>
+			<a href="deconnexion.php">Deconnexion</a>
+		<?php
+	}else{
+		?>
+		<a href="inscription.php">Inscription</a>
+		<a href="login.php">Connexion</a>
+		<?php
+	}
+	?>
+	    </div>
+	  </div>
+	</header>
+  ﻿<body>
+	<div>
+	    ﻿<h1>Profil</h1>
+
+			<?php
+					echo 'Pseudo : ' . $_SESSION['pseudo'] . '<br>' . 'Email : ' . $_SESSION['email'] . '<br>' . 'ID : ' . $_SESSION['id'];
+				
+			?>
+	</div>
+	<div>
+		<br>
+		<br>
+		<a href="postersondage.php">Poster un sondage</a>
+	</div>
+	<div>
+	    ﻿<h1>Sondages :</h1>
+
+			<div><strong>Récents :</strong></div>
+
+			<?php
+
+				$reponse = $pdo->query('SELECT titre, lien, date_creation_sondage FROM Sondage ORDER BY date_creation_sondage DESC');
+
+				while ($donnees = $reponse->fetch())
+				{
+					echo $donnees['titre'] . ' : ' . '<a href="' . $donnees['lien'] . '">' . $donnees['lien'] . '</a>' . ' Posté le : ' . $donnees['date_creation_sondage'] . '<br />';
+				}
+
+				$reponse->closeCursor();
+
+				?>
+
+
+		</div>
+
+	  </body>
 </html>
---
+++ inscription.php
@@ -0,0 +1,158 @@
+<?php
+session_start();
+
+include('./BDD.php');
+
+
+
+  if (isset($_SESSION['id'])){   //si une session existe déja (= utilisateur connecté) on redirige vers la page d'accueil
+    header('Location: index.php');
+    exit;
+  }
+
+if(!empty($_POST)){ //si le formulaire est vide ne rien faire
+    extract($_POST); //extrait les valeurs du form en 4 variables $pseudo $email $mdp $confmdp
+
+
+    $ok = true;
+ 
+    if (isset($_POST['inscription'])){ //test pour le formulaire "inscription"
+
+      //htmlentites = pour éviter les injections, trim = enleve les espaces au début et a la fin
+      $pseudo = htmlentities(trim($pseudo)); 
+      $email = htmlentities(strtolower(trim($email))); 
+      $mdp = trim($mdp); 
+      $confmdp = trim($confmdp); 
+ 
+      // Verif pseudo
+      if(empty($pseudo)){
+        $ok = false;
+        $er_pseudo = ("Le pseudo est vide");
+      }
+      else{
+
+        //Verif que le pseudo existe pas déjà
+		$stmt = $pdo->prepare("SELECT * FROM User WHERE pseudo=?");
+		$stmt->execute([$pseudo]); 
+		$req_pseudo = $stmt->fetch();
+		if ($req_pseudo) {
+			$ok = false;
+			$er_pseudo = "Ce pseudo existe déjà";
+        } 
+    }
+
+   /* elseif(strlen($mdp)<6){
+        $ok = false;
+        $er_mdp = "Le mot de passe doit faire au minimum 6 caractères." */
+
+      // Verif email
+      if(empty($email)){
+        $ok = false;
+        $er_mail = "L'email est vide";
+ 
+       //on verif que le mail est dans un format mail
+      }elseif(!preg_match("/^[a-z0-9\-_.]+@[a-z]+\.[a-z]{2,3}$/i", $email)){
+        $ok = false;
+        $er_email = "L'email est invalide";
+
+      }else{
+      		
+        //On check dans la base de donnée si le mail existe déjà
+		$stmt = $pdo->prepare("SELECT * FROM User WHERE email=?");
+		$stmt->execute([$email]); 
+		$req_email = $stmt->fetch();
+		if ($req_email) {
+			$ok = false;
+			$er_email = "Ce mail existe déjà";
+        } 
+    }
+
+      // Verif du mot de passe
+      if(empty($mdp)) {
+        $ok = false;
+        $er_mdp = "Le mot de passe est vide";
+
+ 	  //verif que la confirmation du mp est valide
+      }elseif($mdp != $confmdp){
+        $ok = false;
+        $er_mdp = "Les deux mots de passe ne correspondent pas";
+      }
+ 
+      //on execute la requete sql si toutes les conditions sont valides
+      if($ok){
+
+        $mdp = crypt($mdp, '$6$rounds=5000$pollexpresslesangdelaveine$'); //cryptage du mdp
+        $datecreation = date('Y-m-d H:i:s');
+
+        $req = $pdo->prepare("INSERT INTO User SET pseudo = :pseudo, motdepasse = :motdepasse, email = :email, date_creation = :datecreation"); 
+        $req->execute(array('pseudo' => $pseudo, 'motdepasse' => $mdp, 'email' => $email, 'datecreation' => $datecreation));
+        
+
+        header('Location: index.php'); //redirection vers la page index.php
+        exit;
+      }
+    }
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="registerlogin.css" media="screen" type="text/css" />
+    <title>Inscription</title>
+  </head>
+  <body>
+    <form method="post">
+    	<div class="container">
+    		<h1>S'inscrire</h1>
+    		<p>Remplissez ce formulaire pour créer votre compte PollExpress.</p>
+    		<hr>
+		      	<?php
+		        if (isset($er_pseudo)){
+		        ?>
+		          <div><?= $er_pseudo ?></div>
+		        <?php 
+		        }
+		      ?>
+		  <label for="pseudo"><b>Pseudo</b></label>
+		  <br>
+	      <input type="text" placeholder="Votre pseudo" name="pseudo" value="<?php if(isset($pseudo)){ echo $pseudo; }?>" id="pseudo"> 
+	      <br>
+
+		      <?php
+		        if (isset($er_email)){
+		        ?>
+		          <div><?= $er_email ?></div>
+		        <?php 
+		        }
+		      ?>
+		  <label for="email"><b>Email</b></label>
+		  <br>
+	      <input type="email" placeholder="Adresse email" name="email" value="<?php if(isset($email)){ echo $email; }?>" id="email">
+	      <br>
+		      <?php
+		        if (isset($er_mdp)){
+		        ?>
+		          <div><?= $er_mdp ?></div>
+		        <?php 
+		        }
+		      ?>
+		  <label for="password"><b>Mot de passe</b></label>
+		  <br>
+	      <input type="password" placeholder="Mot de passe" name="mdp" id="mdp" required>
+	      <br>
+	      <label for="password"><b>Confirmer le mot de passe</b></label>
+	      <br>
+	      <input type="password" placeholder="Confirmer le mot de passe" name="confmdp" id="confmdp" required>
+	      <hr>
+	      <button type="submit" name="inscription" class="boutonform">Envoyer</button>
+      </div>
+      	<div class="container login">
+    		<p>Vous avez déjà un compte ? <a href="login.php">Se connecter</a>.</p>
+  		</div>
+    </form>
+  </body>
+</html>
