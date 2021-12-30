a43479ee3fdfab7584cd3173c07a21f9fabace01
Gaido Tristan <tristan.gaido01@etu.umontpellier.fr>
2021-12-30 14:22:13 +0100

    m

--- view/profil/profil.php
+++ view/profil/profil.php
@@ -24,19 +24,26 @@ $req2->closeCursor();
         <section class="clean-block clean-info dark" style="background: #dcdde1; padding-top: 80px;">
             <div class="container">
                 <div class="block-heading">
-                    <h2 class="text-info" style="color: #0c2461; ">Profil</h2>
-                </div>
-                <div class="row align-items-center">
-                    <div class="col-md-6" style="width: 1000px">
-                        <h3 >Pseudo : <?php echo $_SESSION['pseudo'];?></h3> 
-                        <div class="getting-started-info"></div>
-                        <h3>Argent : <?php echo $_SESSION['argent'];?></h3>
-                        <h3>Email : <?php echo $_SESSION['email'];?> </h3>
-                        <h3>Date de création du compte : <?php echo $_SESSION['date']; ?> </h3>
-                        <h3>Nombre de sondages répondus : <?php echo $resultat2['Stats']; ?></h3>
+                	<div class="tab">
+                	<button class="tablinks" id="defaultOpen" onclick="openCity(event, 'Profil')">Profil</button>
+                	<button class="tablinks"  onclick="openCity(event, 'Equipement')">Equipement</button>  
+                	</div>
+                    <div id="Profil" class="tabcontent">
 
-                        <br><br><br>
+                      <h2 class="text-info" style="color: #0c2461; ">Profil</h2>
+                  
+                  <div class="row align-items-center">
+                      <div class="col-md-6" style="width: 1000px">
+                          <h3 >Pseudo : <?php echo $_SESSION['pseudo'];?></h3> 
+                          <div class="getting-started-info"></div>
+                          <h3>Argent : <?php echo $_SESSION['argent'];?></h3>
+                          <h3>Email : <?php echo $_SESSION['email'];?> </h3>
+                          <h3>Date de création du compte : <?php echo $_SESSION['date']; ?> </h3>
+                          <h3>Nombre de sondages répondus : <?php echo $resultat2['Stats']; ?></h3>
 
+                          <br><br><br></div></div>
+</div></div>
+						<div id="Equipement" class="tabcontent">
                         <h3>Équipement</h3> 
                         <?php 
 
@@ -61,7 +68,7 @@ $req2->closeCursor();
 
                     </div>
                 </div>
-            </div>
+            </div></div>
             <main class="page pricing-table-page">
         <section class="clean-block clean-pricing dark">
             <div class="container">
@@ -89,5 +96,59 @@ $req2->closeCursor();
                                         ?>
                                 
                     </div>
-                </div></div></div>
+                </div></div></div></div></div></section></main></div></section></main>
+
+
+<style type="text/css">
+	.tab {
+	  overflow: hidden;
+	  border: 1px solid #ccc;
+	  background-color: #f1f1f1;
+	}
+
+	.tab button {
+	  background-color: inherit;
+	  border: none;
+	  outline: none;
+	  cursor: pointer;
+	  padding: 14px 16px;
+	  transition: 0.3s;
+	}
+
+	.tab button:hover {
+	  background-color: #ddd;
+	}
+
+	.tab button.active {
+	  background-color: #ccc;
+	}
+
+	.tabcontent {
+	  display: none;
+	  padding: 6px 12px;
+	  border: 1px solid #ccc;
+	  border-top: none;
+	}
+</style>
+<script type="text/javascript">
+	document.getElementById("defaultOpen").click();
+
+	function openCity(evt, cityName) {
+	  var i, tabcontent, tablinks;
+
+	  tabcontent = document.getElementsByClassName("tabcontent");
+	  for (i = 0; i < tabcontent.length; i++) {
+	    tabcontent[i].style.display = "none";
+	  }
+
+	  tablinks = document.getElementsByClassName("tablinks");
+	  for (i = 0; i < tablinks.length; i++) {
+	    tablinks[i].className = tablinks[i].className.replace(" active", "");
+	  }
+
+	  document.getElementById(cityName).style.display = "block";
+	  evt.currentTarget.className += " active";
+	}
+</script>
+
 
